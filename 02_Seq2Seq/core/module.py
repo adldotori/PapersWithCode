e00acbf0a55c19a895c26ad6b281cf6caf6088f5
adldotori <adldotori@gmail.com>
2021-03-02 13:55:53 +0000

    add translator

--- 02_Seq2Seq/core/module.py
+++ 02_Seq2Seq/core/module.py
@@ -27,16 +27,16 @@ torch.backends.cudnn.deterministic = True
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
-class Trainer():
-    NUM_SRC_WORDS = 160000
-    NUM_TRG_WORDS = 80000
+NUM_SRC_WORDS = 160000
+NUM_TRG_WORDS = 80000
 
+class Trainer():
     def __init__(self, args):
         self.args = args
 
         # data
-        train_dataset = FrEnCorpus(args, self.NUM_SRC_WORDS, self.NUM_TRG_WORDS, True)
-        test_dataset = FrEnCorpus(args, self.NUM_SRC_WORDS, self.NUM_TRG_WORDS, False)
+        train_dataset = FrEnCorpus(args, NUM_SRC_WORDS, NUM_TRG_WORDS, True)
+        test_dataset = FrEnCorpus(args, NUM_SRC_WORDS, NUM_TRG_WORDS, False)
         self.vocab = train_dataset.trg_vocab
 
         self.train_data_loader = DataLoader(dataset=train_dataset, 
@@ -47,14 +47,18 @@ class Trainer():
                                 collate_fn=test_dataset.collate_fn)
 
         # model
-        self.model = Seq2Seq(self.NUM_SRC_WORDS, self.NUM_TRG_WORDS).to(device)
+        self.model = Seq2Seq(NUM_SRC_WORDS, NUM_TRG_WORDS).to(device)
 
         # optimizer
         self.optim = optim.Adam(self.model.parameters())
         
         # loss
         self.criterion = nn.CrossEntropyLoss()
-        
+
+        # make directory if not exist data path
+        if not osp.isdir(args.ck_path): 
+            os.makedirs(args.ck_path, exist_ok=True)
+
     def train(self):
         """
         training seq2seq model
@@ -85,6 +89,9 @@ class Trainer():
             # evaluate
             valid_score = self.evaluate()
             print(f'valid score : {valid_score:.3f}')
+
+            torch.save(self.model.state_dict(), 
+                        osp.join(self.args.ck_path, f'{self.args.name}_best.pt'))
 
     def evaluate(self):
         """
@@ -125,43 +132,66 @@ class Trainer():
                     if index == self.vocab(self.vocab.EOS_TOKEN):
                         break
 
-                ouptut = heapq.heappop(heap)
+                output = heapq.heappop(heap)
                 bleu_score += sentence_bleu([trg[0][1:-1].tolist()], output[0][1:-1].tolist())
-                
+
         self.model.train()
         bleu_score /= len(self.test_data_loader)
 
         return bleu_score * 100
 
 
-class Chat():
+class Tranlator():
     def __init__(self, args):
-        # vocabulary load
-        with open(osp.join(args.ck_path, 'mr.p'), 'rb') as f:
-            self.vocab = pickle.load(f)
-        self.vocab_size = self.vocab.idx
-        self.pad_idx = self.vocab(self.vocab.PAD_TOKEN)
+        self.args = args
 
-        self.min_length = args.filter_sizes[-1]
+        # vocabulary load
+        train_dataset = FrEnCorpus(args, NUM_SRC_WORDS, NUM_TRG_WORDS, True)
+        self.vocab = train_dataset.trg_vocab
 
         # model load
-        self.model = TextCNN(self.vocab_size, self.pad_idx, args).to(device)
-        self.model.load_state_dict(torch.load(osp.join(args.ck_path, f'{args.name}_{args.mode}_best.pt')))
+        self.model = Seq2Seq(self.NUM_SRC_WORDS, self.NUM_TRG_WORDS).to(device)
+        self.model.load_state_dict(torch.load(osp.join(args.ck_path, f'{args.name}_best.pt')))
         self.model.eval()
     
-    def talk(self):
+    def translate(self):
         """
         Determines whether a given sentence is positive or negative
         """ 
         while True:
             input_str = preprocess(input('>>> ')) # clean and tokenize sentence
-            input_str = [self.vocab(i) for i in input_str] # change vocab to idx with vocabulary
-            if len(input_str) < self.min_length: # if a give sentence is too short, add padding
-                input_str += [self.pad_idx for i in range(self.min_length - len(input_str))]
+            input_str = [self.vocab.SOS_TOKEN] + input_str + [self.vocab.EOS_TOKEN] 
+            src = [self.vocab(i) for i in input_str] # change vocab to idx with vocabulary
 
-            input_str = torch.tensor(input_str).to(device).unsqueeze(0)
-            predictions = self.model(input_str)
-            print(f'{predictions.item():.2f}')
+            heap = [torch.tensor(((self.vocab(self.vocab.SOS_TOKEN),),)).to(device)] # [1,1]
+
+            while True:
+                # remain the best elements
+                if len(heap) > self.args.beam_size:
+                    tmp = heap.copy()
+                    heap = []
+                    for i in self.args.beam_size:
+                        heapq.heappush(heap, heapq.heappop(tmp))
+
+                for element in heap:
+                    predictions = self.model(src, element)[0] # [seq_len, num_trg_words]
+                    values, indices = torch.topk(predictions[-1], self.args.beam_size)
+
+                    # add to heap
+                    for value, index in zip(values, indices):
+                        heapq.heappush(heap, \
+                            (-value, torch.cat((element, torch.tensor(((index,),)).to(device)), -1))) # maximum heap / [1,x]
+                    
+                if index == self.vocab(self.vocab.EOS_TOKEN):
+                    break
+
+            output = heapq.heappop(heap)
+            
+            seq_trans = ''
+            for word in output[1:-1]: # except sos, eos token
+                seq_trans += self.vocab.idx2word[word]
+
+            print(f'>>> {seq_trans}')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Trainer')
@@ -177,4 +207,5 @@ if __name__ == '__main__':
     trainer = Trainer(args)
     trainer.train()
 
-    chat = Chat(args)
+    translator = Tranlator(args)
+    translator.translate()
